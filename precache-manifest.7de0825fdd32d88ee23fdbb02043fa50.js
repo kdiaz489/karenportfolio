be84c14493c9e0ba23581ab2e92c16820cc7e9af
Karen <kstefdiaz@gmail.com>
2018-11-29 22:45:28 -0800

    Updates

---
+++ precache-manifest.7de0825fdd32d88ee23fdbb02043fa50.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e4ba6628d9556e9f741cde7fad003db",
+    "url": "/karenportfolio/static/media/avatar.6e4ba662.png"
+  },
+  {
+    "revision": "a9ebaebdb50ff069593e5c143dede928",
+    "url": "/karenportfolio/static/media/avatar-round.a9ebaebd.png"
+  },
+  {
+    "revision": "a66abebf982a0f7c0c88",
+    "url": "/karenportfolio/static/js/runtime~main.a66abebf.js"
+  },
+  {
+    "revision": "92237bb7447ef39990e0",
+    "url": "/karenportfolio/static/js/main.92237bb7.chunk.js"
+  },
+  {
+    "revision": "2854bbf69115dd4172bd",
+    "url": "/karenportfolio/static/js/1.2854bbf6.chunk.js"
+  },
+  {
+    "revision": "92237bb7447ef39990e0",
+    "url": "/karenportfolio/static/css/main.6c0d1eeb.chunk.css"
+  },
+  {
+    "revision": "2854bbf69115dd4172bd",
+    "url": "/karenportfolio/static/css/1.00184f9e.chunk.css"
+  },
+  {
+    "revision": "9bed1ccf9b259633d880056f24768bcf",
+    "url": "/karenportfolio/index.html"
+  }
+];
